4738fe088a228bb3aff16968b59a762ef6a456a1
Alesha <a.mostowenko2012@yandex.ru>
2019-01-12 19:55:26 +0300

    Add files via upload

---
+++ default.py
@@ -0,0 +1,415 @@
+# -*- coding: utf-8 -*-
+# Module: default
+# License: GPL v.3 https://www.gnu.org/copyleft/gpl.html
+
+from __future__ import unicode_literals
+import time
+
+import xbmc
+import xbmcgui
+import xbmcplugin
+
+import resources.lib.ntv as ntv
+import simplemedia
+
+# Create plugin instance
+plugin = simplemedia.RoutedPlugin()
+_ = plugin.initialize_gettext()
+
+use_subtitles = plugin.get_setting('use_subtitles')
+
+__handle__ = int(sys.argv[1])
+
+def _init_api():
+
+    settings = {'cache_dir': plugin.profile_dir}
+
+    return ntv.NTV(settings)
+
+
+def _show_api_error(err):
+    plugin.log_error(err)
+    try:
+        text = _(str(err))
+    except simplemedia.SimplePluginError:
+        text = str(err)
+
+    xbmcgui.Dialog().notification(plugin.addon.getAddonInfo('name'), text, xbmcgui.NOTIFICATION_ERROR)
+
+
+def _show_notification(text):
+    xbmcgui.Dialog().notification(plugin.addon.getAddonInfo('name'), text)
+
+
+@plugin.route('/')
+def root():
+    plugin.create_directory(_list_root())
+
+
+def _list_root():
+
+    try:
+        genres = _get_genres()
+    except ntv.NTVApiError as err:
+        _show_api_errosr(err)
+        genres = []
+
+    url = plugin.url_for('play_live')
+    list_item = {'label': 'Live',
+                 'url': url,
+                 'icon': plugin.icon,
+                 'fanart': plugin.fanart,
+                 'plot': 'Прямая трансляция телеканала НТВ',
+                 'content_lookup': False,
+                 'is_folder': False,
+                 'is_playable': True,
+                 }
+    yield list_item
+
+    # url = plugin.url_for('newss')
+    # list_item = {'label': 'News',
+                 # 'url': url,
+                 # 'icon': plugin.icon,
+                 # 'fanart': plugin.fanart,
+                 # 'content_lookup': False,
+                 # 'is_folder': True,
+                 # 'is_playable': False,
+                 # }
+    # yield list_item
+
+    for genre in genres:
+        url = plugin.url_for('genre', genre_title=genre['title'])
+        list_item = {'label': genre['title'],
+                     'url': url,
+                     'icon': plugin.icon,
+                     'fanart': plugin.fanart,
+                     'content_lookup': False,
+                     }
+        yield list_item
+    
+    result = []
+    for news in _api.get_newss():
+        result.append(news)
+    
+    for news in result:
+        #_show_notification(news['title'])
+        if news.get('video_list') is not None:
+            url = plugin.url_for('news', n_id=news['video_list'][0]['hi_video'])
+            #_show_notification(str(__handle__))
+            list_item = {'label': news['title'],
+                 'info': {'video': {
+                                    # 'country': country,
+                                    'title': news['title'],
+                                    'originaltitle': news['title'],
+                                    'plotoutline': news['lead'],	
+                                    'plot': news['lead'],									
+                                    }
+                          },
+                 'art': {'poster': news['img'],
+                         },
+                 'fanart': plugin.fanart,
+                 'thumb': news['img'],
+                 'content_lookup': False,
+                 'is_folder': False,
+                 'is_playable': True,
+                 'url': url,
+                 'path': str(news['video_list'][0]['hi_video']),
+                 }				 
+            #_show_notification(list_item)
+            #yield list_item
+        else:
+            url = plugin.url_for('news', n_id=news['img'])
+            #_show_notification(news['img'])
+            list_item = {'label': news['title'],
+                 'info': {'video': {
+                                    # 'country': country,
+                                    'title': news['title'],
+                                    'originaltitle': news['title'],
+                                    'plotoutline': news['lead'],
+                                    'plot': news['lead'],									
+                                    }
+                          },
+                 'art': {'poster': news['img'],
+                         },
+                 'fanart': plugin.fanart,
+                 'thumb': news['img'],
+                 'content_lookup': False,
+                 'is_folder': False,
+                 'is_playable': True,
+                 'url': url,
+                 'path': news['img'],
+                 }
+        yield list_item
+    
+    # url = plugin.url_for('search')
+    # list_item = {'label': _('Search'),
+                 # 'url': url,
+                 # 'icon': plugin.icon,
+                 # 'fanart': plugin.fanart,
+                 # 'content_lookup': False,
+                 # }
+    # yield list_item
+
+
+@plugin.route('/genre/<genre_title>')
+def genre(genre_title):
+    params = {'offset': plugin.params.offset or 0,
+              'limit': plugin.params.limit or plugin.get_setting('limit'),
+              }
+    update_listing = (params['offset'] > 0)
+    genre_id = _get_genre_id(genre_title)
+    
+    programs_info = _api.browse_programs(genre_id, params)
+
+    plugin.create_directory(_list_programs(programs_info, genre_title), content='movies', category=programs_info['title'], update_listing=update_listing)
+
+@plugin.route('/news/<n_id>')		
+def news(n_id):
+    list_item = {
+                 'path': n_id,
+                 }
+    plugin.resolve_url(list_item)
+    #xbmc.executebuiltin('PlayMedia({})'.format(n_id))
+	
+def _list_programs(data, genre_title):
+
+    mediatype = 'tvshow'
+    for program in data['list']:
+        url = plugin.url_for('program_seasons', prog_id=program['shortcat'])
+
+        list_item = {'label': program['title'],
+                     'info': {'video': {  # 'date': date,
+                                        # 'country': country,
+                                        # 'year': year,
+                                        'title': program['title'],
+                                        'originaltitle': program['title'],
+                                        'sorttitle': program['title'],
+                                        'plotoutline': program['annotation'],
+                                        'plot': program['annotation'],
+                                        'mpaa': program['rating']['mpaa'],
+                                        # 'director': body.get('director', []),
+                                        # 'writer': body.get('writer', []),
+                                        # 'credits': body.get('credits', []),
+                                        'mediatype': mediatype,
+                                        }
+                              },
+                     'art': {'poster': program['img'],
+                             },
+                     'fanart': plugin.fanart,
+                     'thumb':  program['img'],
+                     'content_lookup': False,
+                     'is_folder': True,
+                     'url': url,
+                     }
+        yield list_item
+
+    if data['offset'] > 0:
+        params = {'limit': data['limit']}
+        prev_offset = data['offset'] - data['limit']
+        if prev_offset > 0:
+            params['offset'] = prev_offset
+        url = plugin.url_for('genre', genre_title=genre_title, **params)
+        item_info = {'label': _('Previous page...'),
+                     'url':   url}
+        yield item_info
+
+    if (data['offset'] + data['limit']) < data['total']:
+        params = {'limit': data['limit'],
+                  'offset': data['offset'] + data['limit']}
+        url = plugin.url_for('genre', genre_title=genre_title, **params)
+        item_info = {'label': _('Next page...'),
+                     'url':   url}
+        yield item_info
+
+
+@plugin.route('/seasons/<prog_id>')
+def program_seasons(prog_id):
+
+    seasons_info = _api.browse_seasons(prog_id)
+
+    if seasons_info['count'] == 1:
+        for season in seasons_info['list']:
+            url = plugin.url_for('program_episodes', prog_id=seasons_info['shortcat'], archive_id=season['id'])
+            xbmc.executebuiltin('Container.Update("%s")' % url)
+            return
+            
+    plugin.create_directory(_list_seasons(seasons_info), content='seasons', category=seasons_info['title'])
+
+
+def _list_seasons(data):
+    mediatype = 'season'
+    for season in data['list']:
+        url = plugin.url_for('program_episodes', prog_id=data['shortcat'], archive_id=season['id'])
+
+        list_item = {'label': season['title'],
+                     'info': {'video': {  # 'date': date,
+                                        # 'country': country,
+                                        # 'year': year,
+                                        'title': season['title'],
+                                        'originaltitle': season['title'],
+                                        'sorttitle': season['title'],
+                                        'plotoutline': data['annotation'],
+                                        'plot': data['description'],
+                                        'mpaa': data['rating']['mpaa'],
+                                        # 'director': body.get('director', []),
+                                        # 'writer': body.get('writer', []),
+                                        # 'credits': body.get('credits', []),
+                                        'mediatype': mediatype,
+                                        }
+                              },
+                     'art': {'poster': data['img'],
+                             },
+                     'fanart': plugin.fanart,
+                     'thumb':  data['img'],
+                     'content_lookup': False,
+                     'is_folder': True,
+                     'url': url,
+                     }
+        yield list_item
+
+    
+@plugin.mem_cached(180)
+def _get_genres():
+    result = []
+    for genre in _api.get_genres():
+        result.append(genre)
+
+    return result
+
+    
+@plugin.mem_cached(180)
+def _get_genre_id(genre_title):
+    for genre in _get_genres():
+        if genre['title'] == genre_title:
+            return genre['id']
+
+
+@plugin.route('/episodes/<prog_id>/<archive_id>')
+def program_episodes(prog_id, archive_id):
+
+    episodes_info = _api.browse_episodes(prog_id, archive_id)
+
+    plugin.create_directory(_list_episodes(episodes_info), content='episodes', category=episodes_info['title'],
+                     total_items=episodes_info['count'], sort_methods=_get_sort_methods('episodes', 'date'))
+
+
+def _list_episodes(data):
+    mediatype = 'episode'
+    for episode in data['list']:
+        
+        list_item = _get_item(data, episode)
+        yield list_item
+
+
+def _get_item(data, episode):
+    mediatype = 'episode'
+    url = plugin.url_for('play_video', video_id=episode['id'])
+
+    st_time = time.gmtime(episode['timestamp'])     
+    list_item = {'label': episode['title'],
+                 'info': {'video': {'date': time.strftime('%d.%m.%Y', st_time),
+                                    # 'country': country,
+                                    'year': st_time[0],
+                                    'season': episode['season'],
+                                    'sortseason': episode['season'],
+                                    'episode': episode['episode'],
+                                    'sortepisode': episode['episode'],
+                                    'title': episode['title'],
+                                    'originaltitle': episode['title'],
+                                    'tvshowtitle': episode['program_title'],
+                                    'sorttitle': episode['title'],
+                                    'plotoutline': data.get('annotation', ''),
+                                    'plot': episode['description'],
+                                    'mpaa': episode['rating']['mpaa'],
+                                    'duration': episode['duration'],
+                                    'premiered': time.strftime('%Y-%m-%d', st_time),
+                                    'dateadded': time.strftime('%Y-%m-%d %H:%M:%S', st_time),
+                                    # 'writer': body.get('writer', []),
+                                    # 'credits': body.get('credits', []),
+                                    'mediatype': mediatype,
+                                    }
+                          },
+                 'art': {'poster': episode['img'],
+                         },
+                 'fanart': plugin.fanart,
+                 'thumb':  episode['img'],
+                 'content_lookup': False,
+                 'is_folder': False,
+                 'is_playable': True,
+                 'url': url,
+                 'path': url,
+                 }
+
+    if use_subtitles \
+      and episode['subtitles'] is not None:
+        list_item['subtitles'] = [episode['subtitles']]
+
+    return list_item
+
+    
+@plugin.route('/video/<video_id>')
+def play_video(video_id):
+    video_info = _api.get_video_info(video_id)
+    list_item = _get_item(video_info, video_info['item']) 
+    list_item['path'] = _get_video_path(video_info)
+    plugin.resolve_url(list_item)
+
+    
+@plugin.route('/live')
+def play_live():
+    #_show_notification('Live')
+    live_info = _api.get_live_info()
+    #_show_notification(live_info['date'])
+    list_item = {
+                 'path': live_info['hls'],
+                 }	
+    plugin.resolve_url(list_item)
+    #xbmc.executebuiltin('PlayMedia({})'.format(live_info['xhls']))	
+
+    
+@plugin.route('/search')
+def search():
+    pass
+
+
+def _get_video_path(data):
+
+    video_quality = plugin.get_setting('video_quality')
+    
+    path = ''
+    if (not path or video_quality >= 0) and data['video']:
+        path = data['video']
+    if (not path or video_quality >= 1) and data['hi_video']:
+        path = data['hi_video']
+
+    return path
+
+
+def _get_sort_methods(cat, sort=''):
+    sort_methods = []
+
+    if cat == 'episodes' \
+      and not plugin.get_setting("use_atl_names"):
+        if sort == 'date':
+            sort_methods.append(xbmcplugin.SORT_METHOD_DATEADDED)
+        else:
+            sort_methods.append(xbmcplugin.SORT_METHOD_EPISODE)
+    elif cat == 'search':
+        sort_methods.append({'sortMethod': xbmcplugin.SORT_METHOD_UNSORTED, 'label2Mask': '%Y'})
+        sort_methods.append(xbmcplugin.SORT_METHOD_VIDEO_YEAR)
+        sort_methods.append({'sortMethod': xbmcplugin.SORT_METHOD_TITLE_IGNORE_THE, 'label2Mask': '%Y'})
+    elif cat == 'category':
+        sort_methods.append({'sortMethod': xbmcplugin.SORT_METHOD_UNSORTED, 'label2Mask': '%Y'})
+    else:
+        sort_methods.append(xbmcplugin.SORT_METHOD_UNSORTED)
+
+    return sort_methods
+
+
+def _get_image(image):
+    return image if xbmc.skinHasImage(image) else plugin.icon
+
+
+if __name__ == '__main__':
+    _api = _init_api()
+    plugin.run()
---
+++ resources/lib/ntv.py
@@ -0,0 +1,332 @@
+# -*- coding: utf-8 -*-
+# Module: ntv
+# License: GPL v.3 https://www.gnu.org/copyleft/gpl.html
+
+from __future__ import unicode_literals
+
+import requests
+import urllib
+import re
+import random
+
+from future.utils import PY3, iteritems
+
+if PY3:
+    basestring = str
+
+
+class NTVApiError(Exception):
+    """Custom exception"""
+    pass
+
+
+class NTV(object):
+
+    def __init__(self, params=None):
+        params = params or {}
+
+        api_url = 'http://www.ntv.ru/m/v10'
+
+        self._actions = {'main': api_url + '/pr',
+                         'program': api_url + '/prog/#prog_id',
+                         'video': api_url + '/v/#video_id',
+                         'archive': api_url + '/prog/#prog_id/archive/#archive_id',
+                         'stream': 'http://www.ntv.ru/services/m/air',
+						 'news': api_url,
+                         }
+
+        self._headers = {'User-Agent': 'ru.ntv.client_4.5.1',
+                         'Accept-Encoding': 'gzip',
+                         'Connection': 'keep-alive',
+                         }
+
+    def _http_request(self, action, params=None, url_params=None):
+        params = params or {}
+
+        action_settings = self._actions.get(action)
+        
+        if isinstance(action_settings, dict):
+            url = action_settings['url']
+        else:
+            url = action_settings
+
+        if url_params is not None:
+            for key, val in iteritems(url_params):
+                url = url.replace('#{0}'.format(key), str(val))
+
+        try:
+            r = requests.get(url, params=params, headers=self._headers)
+            r.raise_for_status()
+        except requests.ConnectionError:
+            raise NTVApiError('Connection error')
+
+        return r
+
+    @staticmethod
+    def _extract_json(r):
+        try:
+            json = r.json()
+        except ValueError as err:
+            raise NTVApiError(err)
+
+        return json
+
+    @staticmethod
+    def _get_menu(data, menu_type):
+        menus = data['menus']
+        
+        result = []
+        for menu in menus:
+            if menu['type'] == menu_type:
+                result.append(menu['data'])
+        
+        return result
+
+    def get_genres(self):
+        r = self._http_request('main')
+
+        json = self._extract_json(r)
+
+        for index, genre in enumerate(json['data']['genres']):
+            item = {'title': genre['title'],
+                    'id': index,
+                    }
+
+            yield(item)
+
+    def browse_programs(self, genre_id, params=None):
+        params = params or {}
+
+        offset = int(params.get('offset', '0'))
+        limit = int(params.get('limit', '10'))
+
+        if isinstance(genre_id, basestring):
+            genre_id = int(genre_id)
+
+        r = self._http_request('main')
+
+        json = self._extract_json(r)
+
+        genre = json['data']['genres'][genre_id]
+        
+        result = {'count': min(limit, len(genre['programs']) - offset),
+                  'total': len(genre['programs']),
+                  'offset': offset,
+                  'limit': limit,
+                  'title': genre['title'],
+                  'list': self._programs_list(genre['programs'], offset, limit),
+                  }
+        return result
+
+    @staticmethod
+    def _programs_list(programs, offset, limit):
+        for program in programs[offset:(offset + limit)]:
+			
+            annon = program['annotation']
+            
+            if program['archived'] == False:
+                annon = annon + '\n\n ' + program['outtime'].replace('\n', '')
+            else:
+                annon = annon + '\n\n Показ прекращён'
+            
+            item = {'annotation': annon,
+                    'id': program['id'],
+                    'img': program['img'],
+                    'shortcat': program['shortcat'],
+                    'rating': NTV._get_rating(program['r']),
+                    'title': program['title'],
+                    }
+            yield(item)
+
+    def browse_seasons(self, prog_id):
+
+        url_params = {'prog_id': prog_id}
+
+        r = self._http_request('program', url_params=url_params)
+        json = self._extract_json(r)
+
+        data = json['data']
+
+        abouts = self._get_menu(data, 'about')
+        if abouts:
+            description = abouts[0]['txt']
+        else:
+            description = ''
+        
+        archives = self._get_menu(data, 'archive')
+        
+        result = {'count': len(archives),
+                  'title': data['title'],
+                  'type': data['type'],
+                  'shortcat': data['shortcat'],
+                  'rating': self._get_rating(data['r']),
+                  'annotation': data['annotation'],
+                  'description': description,
+                  'img': data['preview'],
+                  'list': self._season_list(archives)
+                  }
+
+        return result
+    
+    @staticmethod
+    def _season_list(archives):
+        for archive in archives:
+            item = {'title': archive['title'],
+                    'id': archive['id'],
+                    }
+            yield item
+
+    def browse_episodes(self, prog_id, archive_id):
+
+        issues = []
+
+        url_params = {'prog_id': prog_id,
+                      'archive_id': archive_id}
+
+        u_params = {'limit': 100, 'offset': 1}
+        
+        while True:
+            r = self._http_request('archive', u_params, url_params)
+            json = self._extract_json(r)
+    
+            data = json['data']
+            archive = data.get('archive')
+            if archive is None:
+                break
+
+            issue_count = archive['issue_count']
+            
+            for issue in archive['issues']:
+                issues.append(issue)
+            
+            u_params['offset'] += u_params['limit']
+            
+            if issue_count < u_params['offset']:
+                break 
+
+        issues.sort(key=NTV._sort_by_ts)
+
+        result = {'count': len(issues),
+                  'title': data['title'],
+                  'type': data['type'],
+                  'shortcat': data['shortcat'],
+                  'rating': self._get_rating(data['r']),
+                  'annotation': data['annotation'],
+                  'list': self._episode_list(issues)
+                  }
+
+        return result
+
+    @staticmethod
+    def _episode_list(issues):
+
+        for issue in issues:
+            if len(issue['video_list']) == 1:
+                video = issue['video_list'][0]
+                yield NTV._video_item(issue, video)
+            else:
+                for part, video in enumerate(issue['video_list']):
+                    yield NTV._video_item(issue, video, part + 1)
+
+    @staticmethod
+    def _video_item(issue, video, part=None):
+
+        item = {'program_title': issue.get('program_title', ''),
+                'title': issue.get('title', ''),
+                'description': issue.get('txt', ''),
+                'rating': NTV._get_rating(video['r']),
+                'allowed': video['allowed'],
+                'img': video['img'],
+                'id': video['id'],
+                'timestamp': float(video['ts']) / 1000
+                ,
+                'duration': video['tt'],
+                'subtitles': video.get('subtitles'),
+                'episode': None,  # NTV._comScore_val(video['comScore'], 'ns_st_en'),
+                'season': None,  # NTV._comScore_val(video['comScore'], 'ns_st_sn'),
+                'genre': None,  # NTV._comScore_val(video['comScore'], 'ns_st_ge'),
+                'part': part,
+                # 'date': video['comScore']['ns_st_ddt'],
+                }
+        return item
+
+    @staticmethod
+    def _sort_by_ts(item):
+        return item['ts']
+
+    @staticmethod
+    def _comScore_val(data, key):
+        data = data or {}
+        
+        if data.get(key) is not None \
+          and data[key] != "*null" :
+            value = data[key] 
+        else:
+            value = None
+        
+        return value
+
+    def get_video_info(self, video_id):
+
+        url_params = {'video_id': video_id}
+
+        r = self._http_request('video', url_params=url_params)
+        json = self._extract_json(r)
+
+        info = json['info']
+        if info['linked_entities'].get('linked_issues') is not None:
+            issue = info['linked_entities']['linked_issues'][0]
+        else:
+            issue = {}
+        result = {'item': NTV._video_item(issue, info),
+                  'video': info['video'],
+                  'hi_video': info.get('hi_video', ''),
+            }
+        return result
+	
+    def get_live_info(self):
+        
+        r = self._http_request('stream')
+        json = self._extract_json(r)
+        
+        
+        return json
+    
+    def get_newss(self):
+        
+        r = self._http_request('news')
+        json = self._extract_json(r)
+        
+        
+        return json['data']['topnews']
+    
+    def _get_season(self, title):
+        parts = title.split('-')
+        if parts[-1].isdigit():
+            result = int(parts[-1])
+        else:
+            result = 1
+        return result
+
+    @staticmethod
+    def _get_rating(rating):
+        rars = rating['v']
+        if rating['k'] <= 0:
+            mpaa = 'G'
+            rars = '0+'
+        elif rating['k'] == 1:
+            mpaa = 'PG'
+        elif rating['k'] == 2:
+            mpaa = 'PG-13'
+        elif rating['k'] == 3:
+            mpaa = 'R'
+        elif rating['k'] == 4:
+            mpaa = 'NC-17'
+        else:
+            mpaa = ''
+        
+        result = {'rars': rars,
+                  'mpaa': mpaa,
+                  }
+        
+        return result
